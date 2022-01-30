e8b5fe59a4321eca9040757b1947c180a65a17ff
Amit <Amit@AMishra-ITG>
2022-01-30 11:40:40 +0530

    Added object equality

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,17 +1,37 @@
 package guru.springframework;
 
+import java.util.Objects;
+
 /**
  * Created by jt on 2018-10-04.
  */
 public class Dollar {
 
-    int amount;
+	private final int amount;
 
-    public Dollar(int amount) {
-        this.amount = amount;
-    }
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
 
-    Dollar times( int multiplier) {
-        return new Dollar(amount * multiplier);
-    }
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(amount);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dollar other = (Dollar) obj;
+		return amount == other.amount;
+	}
+
 }
