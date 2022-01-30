365912768124a1b2015a69d5e7f683552833e9e5
Amit <Amit@AMishra-ITG>
2022-01-30 15:04:16 +0530

    Further Refactoring and adding abstraction

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -5,11 +5,11 @@ package guru.springframework;
  */
 public class Dollar extends Money {
 
-    public Dollar(int amount) {
-        this.amount = amount;
-    }
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
 
-    Dollar times( int multiplier) {
-        return new Dollar(amount * multiplier);
-    }
+	public Money times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -3,11 +3,21 @@ package guru.springframework;
 /**
  * Created by jt on 2018-10-05.
  */
-public class Money {
-    protected int amount;
+public abstract class Money {
+	protected int amount;
 
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount;
-    }
+	public abstract Money times(int multiplier); 
+	
+	public static Money dollar(int value) {
+		return new Dollar(value);
+	}
+
+	public static Money franc(int value) {
+		return new Franc(value);
+	}
+
+	public boolean equals(Object object) {
+		Money money = (Money) object;
+		return amount == money.amount && this.getClass().equals(object.getClass());
+	}
 }
