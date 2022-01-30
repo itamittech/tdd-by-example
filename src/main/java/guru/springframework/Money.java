3b9dd5711ac4ca640fe5145cd8215e5a2c159651
Amit <Amit@AMishra-ITG>
2022-01-30 16:46:51 +0530

    Complete Refactoring, subclasses removed

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -3,32 +3,33 @@ package guru.springframework;
 /**
  * Created by jt on 2018-10-05.
  */
-public abstract class Money {
-    protected int amount;
-    protected String currency;
+public class Money {
+	protected int amount;
+	protected String currency;
 
-    public Money(int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
-    }
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 
-    protected String currency() {
-        return currency;
-    }
+	protected String currency() {
+		return currency;
+	}
 
-    public abstract Money times(int multiplier);
+	public Money times(int multiplier) {
+		return new Money(this.amount * multiplier, this.currency);
+	}
 
-    public static Money dollar(int amount){
-        return new Dollar(amount, "USD");
-    }
+	public static Money dollar(int amount) {
+		return new Money(amount, "USD");
+	}
 
-    public static Money franc(int amount){
-        return new Franc(amount, "CHF");
-    }
+	public static Money franc(int amount) {
+		return new Money(amount, "CHF");
+	}
 
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount
-                && this.getClass().equals(object.getClass());
-    }
+	public boolean equals(Object object) {
+		Money money = (Money) object;
+		return amount == money.amount && this.currency.equals(money.currency);
+	}
 }
